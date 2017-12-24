749662fd63e5e7fbe76f19656f8d6e11d77535b5
Sagar V Belavadi <sagar28.05.1997@gmail.com>
2017-12-24 16:09:12 +0530

    Made our rocket cool with drawings xD

--- js/rocket.js
+++ js/rocket.js
@@ -2,12 +2,21 @@ var timer = null;
 var changeState = function (state) {
 	document.body.className = "body-state"+state;
 	clearInterval(timer);
+	document.getElementById('nervous').className = 'nervous';
+	document.getElementById('cant-wait').className = 'cant-wait';
 
 	if(state == 2){
 		document.getElementById('countdown').innerHTML = 10;
 		timer = setInterval(function () {
-			document.getElementById('countdown').innerHTML -= 1; 
+			document.getElementById('countdown').innerHTML -= 1;
+			if(document.getElementById('countdown').innerHTML == 6){
+				document.getElementById('nervous').className = 'nervous show';
+			}else if(document.getElementById('countdown').innerHTML == 3){
+				document.getElementById('nervous').className = 'nervous';
+				document.getElementById('cant-wait').className = 'cant-wait show';
+			}
 			if(document.getElementById('countdown').innerHTML <= 0){
+				document.getElementById('cant-wait').className = 'cant-wait';				
 				changeState(3);
 			}
 		}, 1000);
@@ -17,7 +26,7 @@ var changeState = function (state) {
 
 			console.log('randomNumber: '+randomNumber);
 
-			if(randomNumber > 5){
+			if(randomNumber > 3){
 				changeState(4); //Success!
 			}else{
 				changeState(5);//Oh No!
