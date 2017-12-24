ab86fd141f67d1e1fd9eddf07c5f437287bd15a9
Sagar V Belavadi <sagar28.05.1997@gmail.com>
2017-12-24 13:57:12 +0530

    Made my rocket!!! xD

---
+++ js/rocket.js
@@ -0,0 +1,27 @@
+var timer = null;
+var changeState = function (state) {
+	document.body.className = "body-state"+state;
+	clearInterval(timer);
+
+	if(state == 2){
+		document.getElementById('countdown').innerHTML = 10;
+		timer = setInterval(function () {
+			document.getElementById('countdown').innerHTML -= 1; 
+			if(document.getElementById('countdown').innerHTML <= 0){
+				changeState(3);
+			}
+		}, 100);
+	}else if(state == 3){
+		var success = setTimeout(function(){
+			var randomNumber = Math.round(Math.random()*10);
+
+			console.log('randomNumber: '+randomNumber);
+
+			if(randomNumber > 5){
+				changeState(4); //Success!
+			}else{
+				changeState(5);//Oh No!
+			}
+		},2000)
+	}
+}
